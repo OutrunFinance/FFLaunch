3b4a54fcc933fb756d19d5aebebb0c8c30f1747d
Jason <2045330878@qq.com>
2024-03-15 18:42:51 +0800

    [opt] update IRETHStakeManager

--- src/core/utils/IRETHStakeManager.sol
+++ src/core/utils/IRETHStakeManager.sol
@@ -5,5 +5,5 @@ pragma solidity ^0.8.24;
  * @title IRETHStakeManager interface
  */
 interface IRETHStakeManager {
-    function stake(uint256 amountInRETH, uint256 lockupDays, address positionOwner, address receiver) external;
+    function stake(uint256 amountInRETH, uint256 lockupDays, address positionOwner, address pethTo, address reyTo) external returns (uint256, uint256);
 }
