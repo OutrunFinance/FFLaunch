e9d64e920fbf521904b0af35b294853b6839c156
Jason <2045330878@qq.com>
2024-03-24 16:07:56 +0800

    [opt] redeploy

--- script/FFLaunchScript.s.sol
+++ script/FFLaunchScript.s.sol
@@ -8,11 +8,11 @@ contract FFLaunchScript is BaseScript {
     function run() public broadcaster {
         EthFFLauncher ethLauncher = new EthFFLauncher(
             0xcae21365145C467F8957607aE364fb29Ee073209,
-            0xdaC9Ed63dada8A7005ce2c69F8FF8bF6C272a3D0,
-            0x71e6A18c57F8794134A8e7088A61bBec22Cf1777,
-            0x7a90a8d701584e9029c14b444a519eC33567F388,
-            0x5A32bca57480f0B9910EcDB8DB854649b1E4F38C,
-            0xC5Bb4e3C1e6143E6d70D65Ce51CA43f5da02dF24
+            0x8921b78E6b521dF5F55eF41e1787100BD43c1366,
+            0xB4A206bF720B27551Afc70be203562caEE0AEd45,
+            0x1E7EC127f64d0dB0670d2F084Cdc02e74A6dCcc5,
+            0x56e36C75899Ad410f90582c0AD2354eAE8F6952D,
+            0x0F487DF3E7C641F422a0a28Dc419C416e9fda95E
         );
         address ethLauncherAddress = address(ethLauncher);
 
