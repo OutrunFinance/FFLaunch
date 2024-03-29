99011cee5a54b372118c5ba020ca594b15dac0d5
Jason <2045330878@qq.com>
2024-03-29 19:01:46 +0800

    [add] add example deploy script

---
+++ script/ExampleScript.s.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+import "./BaseScript.s.sol";
+import "../src/example/FF.sol";
+import "../src/example/FFPoolCallee.sol";
+
+contract FFLaunchScript is BaseScript {
+    function run() public broadcaster {
+        address owner = vm.envAddress("OWNER");
+        address PETH = vm.envAddress("PETH");
+        address launcher = vm.envAddress("LAUNCHER");
+        address gasManager = vm.envAddress("GAS_MANAGER");
+
+        FFPoolCallee callee = new FFPoolCallee(
+            owner,
+            PETH,
+            launcher,
+            gasManager,
+            block.timestamp + 3 days,
+            block.timestamp + 6 days
+        );
+        address calleeAddress = address(callee);
+        FF ff = new FF(launcher, calleeAddress, gasManager);
+        address ffAddress = address(ff);
+        callee.initialize(ffAddress);
+
+        console.log("FFPoolCallee deployed on %s", calleeAddress);
+        console.log("FF deployed on %s", ffAddress);
+    }
+}
