7dcce8ac058282096ee0de04b7aca358307c2355
Jason <2045330878@qq.com>
2024-04-18 18:20:58 +0800

    [opt] Change nextId to internal

--- src/core/utils/AutoIncrementId.sol
+++ src/core/utils/AutoIncrementId.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.24;
 abstract contract AutoIncrementId {
     uint256 public id = 0;
 
-    function nextId() public returns (uint256) {
+    function nextId() internal returns (uint256) {
         ++id;
         return id;
     }
