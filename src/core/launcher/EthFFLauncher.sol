dbd721f2b68f76ff39746d8596b4f4ecc371e23e
Jason <2045330878@qq.com>
2024-04-18 18:20:23 +0800

    [opt] format

--- src/core/launcher/EthFFLauncher.sol
+++ src/core/launcher/EthFFLauncher.sol
@@ -6,7 +6,6 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-
 import "./interfaces/IEthFFLauncher.sol";
 import "../utils/IRETH.sol";
 import "../utils/AutoIncrementId.sol";
